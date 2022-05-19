30392fc06e00adce7e0223ff5594d51b1541976d
suriyaprks91 <suriyaprks91@gmail.com>
2022-05-19 08:22:12 -0700

    new project

---
+++ src/program/StringReverse.java
@@ -0,0 +1,19 @@
+package program;
+
+public class StringReverse {
+	
+	public static void main(String[] args) {
+		
+		String str = "Suriya";
+		String rev = "";
+		
+		int len = str.length();
+		
+		for(int i=len-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+	    
+	}
+
+}
